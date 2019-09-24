18751dbf838850179fd914a491f215ae86de3a63
Brian <brian.nickila@gmail.com>
2019-09-24 18:14:04 -0500

    set paths for logging.yml and app.ini files

--- auto_sync/app.py
+++ auto_sync/app.py
@@ -2,18 +2,26 @@ import logging.config
 import os
 
 import yaml
-
+import sys
 from auto_sync.config import ConfigLoader
 from auto_sync.sync import Sync
 from auto_sync.util import read_file
 from auto_sync.worker import Worker
 
+print(os.getcwd())
+if getattr(sys, 'frozen', False):
+    bundle_dir = sys._MEIPASS
+else:
+    bundle_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+
 # Load all properties from INI and point to the folder where resources are stored
-props_file = "app.ini"
+props_file = os.path.join(bundle_dir, "app.ini")
 cl = ConfigLoader.load(props_file, 'sync_resource')
 
 # Just for process output
-with open("logging.yml") as log_cfg:
+with open(os.path.join(bundle_dir, "logging.yml")) as log_cfg:
     logging.config.dictConfig(yaml.safe_load(log_cfg))
 
 sync_options = cl.get_config('sync')
@@ -34,4 +42,3 @@ for config in example_sync_data:
     w = Worker(sync, template_config)
     w.run()
 
-# Some change
