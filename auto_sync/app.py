21e8ab6c51829e90c6a76e69761a8f007df07283
Brian <brian.nickila@gmail.com>
2019-09-26 17:07:21 -0500

    Set it up to check to see if app.ini and example.yml files are in the folder otherwise it creates them and exits. the next time it opens, it uses them.

--- auto_sync/app.py
+++ auto_sync/app.py
@@ -1,6 +1,6 @@
 import logging.config
 import os
-
+from shutil import copyfile
 import yaml
 import sys
 from auto_sync.config import ConfigLoader
@@ -9,6 +9,7 @@ from auto_sync.util import read_file
 from auto_sync.worker import Worker
 
 print(os.getcwd())
+
 if getattr(sys, 'frozen', False):
     bundle_dir = sys._MEIPASS
 else:
@@ -16,9 +17,23 @@ else:
 
 
 # Load all properties from INI and point to the folder where resources are stored
-props_file = os.path.join(bundle_dir, "app.ini")
-if os.path.exists('app.ini'):
+if os.path.exists('app.ini') and os.path.exists('example.yml'):
     props_file = 'app.ini'
+else:
+    generate_files = input("'app.ini' and 'example.yml' not found. Would you like to create them now? y/n: ")
+    generate_files.lower()
+    if generate_files == 'y':
+        copyfile(bundle_dir + '\\app.ini', 'app.ini')
+        copyfile(bundle_dir + '\\example.yml', 'example.yml')
+        print('Files have been created. Open files and modify.')
+        sys.exit()
+    else:
+    # prompt do you want to generate example files? yes: create and exit, no: exit
+    # create copy of app.ini from where it is to the same folder shutil copy
+    # do the same for the example.yml file
+        print('The files have not been created.')
+        props_file = os.path.join(bundle_dir, "app.ini")
+        sys.exit()
 cl = ConfigLoader.load(props_file, '.')
 print(props_file)
 # cl = ConfigLoader.load(props_file, '')
@@ -37,7 +52,8 @@ template_config = resources_config.merge_with(sync_options.values)
 template_config.set_value("log_folder", os.path.abspath(log_folder))
 
 # Read in the set of example syncs
-example_sync_data = read_file("../example_sync/example.yml")
+
+example_sync_data = read_file("example.yml")
 
 # Execute (single thread for now)
 for config in example_sync_data:
